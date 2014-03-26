387581ff5e33df5a090d79ed80d59b01e9bd5109
Caitlin Maclatte <macleod.caitlin@gmail.com>
2014-03-26 20:39:10 +1100

    switching to ubuntal vm

--- myshell.c
+++ myshell.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.g>
 
-// hello i am c
+#define MAX_LENGTH 1024
+#define DELIMITERS " /n/t"
+
+const char *search_dir[] = { "/bin", "/usr/bin", ""};
+
+int main (int argc, char** argv)
+{
+    // define line
+    char input[MAX_LENGTH];
+    char *cmd;
+
+    // infinite loop
+    while (1) {
+        printf("c: ");
+        if (!fgets(input, MAX_LENGTH, stdin)) { break; }
+        // input is now in input
+
+        //parse line
+        if ((cmd = strtok(input, DELIMITERS))) {
+            printf("%s\n", cmd);
+        }
+    }
+}
